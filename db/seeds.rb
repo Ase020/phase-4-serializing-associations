9a18569cc786612843742e8d17c05ea4396a42d9
felixNyalenda <felixasejunior@gmail.com>
2023-06-22 22:58:25 +0300

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+puts "🌱 Seeding spices..."
+
 director1 = Director.create(
   name: "Steven Spielberg",
   birthplace: "Cincinnati, OH",
@@ -18,7 +20,7 @@ movie1.reviews.create([
   {
     author: "Roger Ebert",
     date: "December 20, 1985",
-    url: "https://www.rogerebert.com/reviews/the-color-purple-1985"        
+    url: "https://www.rogerebert.com/reviews/the-color-purple-1985"
   },
   {
     author: "Variety Staff",
@@ -63,3 +65,5 @@ director3.movies.create(
   category: "Drama",
   discount: false
 )
+
+puts "✅ Done seeding!"
